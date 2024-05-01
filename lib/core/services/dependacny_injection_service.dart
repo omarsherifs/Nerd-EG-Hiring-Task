6d7100974cbf73b2ad29c8b59f88197e8baa8f16
Omar Sherif <omar.sherifs@live.com>
2024-05-02 00:04:23 +0300

    add dependency injection

--- lib/core/services/dependacny_injection_service.dart
+++ lib/core/services/dependacny_injection_service.dart
@@ -1,36 +1,34 @@
 import 'package:get_it/get_it.dart';
+import 'package:nerd_eg_hiring_task/features/recipes/data/data_sources/base_recipe_data_source.dart';
+import 'package:nerd_eg_hiring_task/features/recipes/data/data_sources/remote_recipe_data_source.dart';
+import 'package:nerd_eg_hiring_task/features/recipes/data/repository/recipe_repository_impl.dart';
+import 'package:nerd_eg_hiring_task/features/recipes/domain/repository/base_recipes_repository.dart';
+import 'package:nerd_eg_hiring_task/features/recipes/domain/usecase/get_all_recipes_usecase.dart';
 
 final sl = GetIt.instance;
 
 class DependencyInjectionServices {
   void init() {
-    initHome();
+    initDependencies();
   }
 
-  void initHome() {
-    // // init Repositories
-    // sl
-    //   ..registerLazySingleton<BaseListHrEmployeeRepository>(
-    //     () => ImplListHrEmployeeRepository(sl()),
-    //   )
-    //   ..registerLazySingleton<BaseListItEmployeeRepository>(
-    //     () => ImplListItEmployeeRepository(sl()),
-    //   )
-    //   // init Use Cases
-    //   ..registerLazySingleton<ListHrDepartmentEmployeesUseCase>(
-    //     () => ListHrDepartmentEmployeesUseCase(
-    //       baseListHrEmployeeRepository: sl(),
-    //     ),
-    //   )
-    //   ..registerLazySingleton<ListItDepartmentEmployeesUseCase>(
-    //     () => ListItDepartmentEmployeesUseCase(
-    //       baseListItEmployeeRepository: sl(),
-    //     ),
-    //   )
+  void initDependencies() {
+    // init Repositories
+    sl
+      ..registerLazySingleton<BaseRecipeRepository>(
+        () => RecipeRepository(sl()),
+      )
 
-    //   // init Data Sources
-    //   ..registerLazySingleton<BaseEmployeesDataSource>(
-    //     RemoteEmployeesDataSource.new,
-    //   );
+      // init Use Cases
+      ..registerLazySingleton<GetRecipeUseCase>(
+        () => GetRecipeUseCase(
+          sl(),
+        ),
+      )
+
+      // init Data Sources
+      ..registerLazySingleton<BaseRecipeDataSource>(
+        RemoteRecipeDataSource.new,
+      );
   }
 }
